6056b608dafa71b8b4469b1275ac1d0776b48d98
Kumar <sk895@exeter.ac.uk>
2025-06-09 15:37:11 +0100

    Remove legacy py-simPace directory after migrating to pysimpace

--- py-simPace/__init__.py
+++
@@ -1,9 +0,0 @@
-## __init__.py
-
-from .motion_simulation import MotionSimulation
-from .mri_loader import MRILoader
-from .artifact_analysis import ArtifactAnalysis
-from .motion_correction import MotionCorrection
-from .utils import Utils
-
-__all__ = ["MotionSimulation", "MRILoader", "ArtifactAnalysis", "MotionCorrection", "Utils"]
